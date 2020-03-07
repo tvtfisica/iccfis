99211a098dd3c507aa7484b1c2ea8bade243069b
karkaroto <vasconcelos.tago@gmail.com>
2020-03-07 16:11:08 -0300

    vai logo

---
+++ primeira_aula.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void main() {
+    printf("hello word!!!\n");   //vai printar hello word; \n vai pular linha
+    printf("soma: %d\n", 2+2);  // o %d vai deixar o numeor inteiro
+    printf("multiplicacao: %d\n", 45*3);
+    printf("subtracao: %d\n", 30-10);
+    printf("divisao: %d\n", 12/3);
+
+}
